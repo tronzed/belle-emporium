65125c390747ba1d5e67ed3f9a91f8d62106cd72
tanuj <you@3example3.com>
2024-08-08 03:16:15 +0530

    test

---
+++ assets/js/custom.js
@@ -0,0 +1,21 @@
+
+// testimonial carousel
+$('#testimonial_carousel').owlCarousel({
+loop:true,
+margin:10,
+nav:false,
+dots:true,
+responsive:{
+        0:{
+            items:1
+        },
+        760:{
+            items:1
+        },
+        1170:{
+            items:1
+        }
+    }
+})
+
+
